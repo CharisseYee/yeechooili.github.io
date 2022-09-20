6721c208ed87f42a34c5b6e19df7f3b25a165e82
YeeChooiLi <80748969+CharisseYee@users.noreply.github.com>
2022-09-20 15:41:03 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee1.cmilbn5pgstu.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "aws_user"
-customdb = "employee"
-custombucket = "yeechooili-employee"
-customregion = "us-east-1"

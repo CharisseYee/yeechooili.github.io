be2516ea0d99dcafd8e660df2b672bcf8b7e570d
ChooiLi <yeechooili1120@gmail.com>
2022-09-26 08:10:39 +0800

    getemp

--- EmpApp.py
+++ EmpApp.py
@@ -30,6 +30,14 @@ def home():
 def about():
     return render_template('AboutUs.html')
 
+@app.route("/getemp", methods=['GET','POST'])
+def getEmp():
+    select_emp = "SELECT * from employee where emp_id = 'emp_id'"
+    cursor = db_conn.cursor()
+    emp_id = request.form['emp_id']
+    cursor.execute(select_emp, emp_id)
+    return render_template('GetEmp.html')
+    
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():

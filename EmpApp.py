ba5c2d72772d0d660ff7f8b595f16eeb6aedae63
ChooiLi <yeechooili1120@gmail.com>
2022-09-26 09:29:20 +0800

    try

--- EmpApp.py
+++ EmpApp.py
@@ -35,7 +35,7 @@ def getEmp():
     return render_template('GetEmp.html')
 
 @app.route("/fetchdata", methods=['GET','POST'])
-def fetchdata(emp_id):
+def fetchdata(AddEmp):
     emp_id = AddEmp.emp_id
     select_emp = "SELECT * from employee where emp_id = %s"
     cursor = db_conn.cursor()

317893a869476a803f65bdcbcce79f08e9593e6c
ChooiLi <yeechooili1120@gmail.com>
2022-09-26 19:46:43 +0800

    alter

--- EmpApp.py
+++ EmpApp.py
@@ -52,8 +52,6 @@ def GetEmp():
         s3 = boto3.resource('s3')
 
         try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
